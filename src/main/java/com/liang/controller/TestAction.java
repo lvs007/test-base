36a144549642a7dcdd9057d422cb2e8be381d5f0
liangzhiyan <liangzhiyan@mucang.cn>
2017-02-04 17:31:39 +0800

    调整代码结构 1、加上tomcat的参数配置 2、加上获取全局变量的依赖注入，和去掉多余的代码

--- src/main/java/com/liang/controller/TestAction.java
+++ src/main/java/com/liang/controller/TestAction.java
@@ -1,10 +1,14 @@
 package com.liang.controller;
 
+import com.liang.service.UserService;
+import liang.mvc.commons.LogUtils;
+import liang.mvc.commons.SpringContextHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import com.liang.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 /**
  * Created by mc-050 on 2016/11/21.
@@ -12,12 +16,13 @@ import com.liang.service.UserService;
 @Controller
 public class TestAction {
 
-    public TestAction(){
-        System.out.println("[TestAction]create TestAction");
-    }
+    private static final LogUtils.Log LOG = LogUtils.getInstance(TestAction.class);
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private SpringContextHolder contextHolder;
 
     @ResponseBody
     public Object test() {
@@ -26,15 +31,22 @@ public class TestAction {
 
     @ResponseBody
     public Object getPerson() {
+        String param = contextHolder.getRequest().getParameter("test");
+        Enumeration<String> attributes = contextHolder.getContext().getAttributeNames();
+        while (attributes.hasMoreElements()) {
+            System.out.println("element:" + attributes.nextElement());
+        }
+        System.out.println("获取的参数是：" + param);
         return userService.getPerson();
     }
 
     @ResponseBody
-    public Object getPersonList(){
+    public Object getPersonList() {
+        LOG.debugLog("[TestAction]请求getPersonList");
         return userService.getPersonList();
     }
 
-    public Object getValue(){
+    public Object getValue() {
         return "value hhhh";
     }
 
